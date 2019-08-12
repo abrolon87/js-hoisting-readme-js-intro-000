a86421bfc08be7b5ace290f35ef9691690c9951c
Amanda R <abrolon87@gmail.com>
2019-08-12 04:46:03 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,27 +1,30 @@
-
 function callMe() {
-
-  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
   return lyric;
+  var lyric = "maybe";
 }
 
-function crazy() {
- thisIsCrazy();
- var thisIsCrazy = function() {
-  console.log("hey!!!")// fix the code in here:
 
+function crazy() {
+  // fix the code in here:
+  thisIsCrazy();
+
+  var thisIsCrazy = function (){
+    console.log("hey!!!")
   }
+}
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Kristin";
+  var name = "Cricky";
 
+  sayMy();
 
-
-   sayMy();
-
-   function sayMy() {
-     
-     console.log(name);
-}
+  function sayMy() {
+    console.log(name);
+    var name = "Kristin";
+  }
 }
