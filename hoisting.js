3854cfaa5e902633449eda6a042a76557e88f479
Amanda R <abrolon87@gmail.com>
2019-08-12 05:02:04 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -9,21 +9,3 @@ function callMe() {
 }
 //passed
 
-function crazy() {
-  thisIsCrazy
-  thisIsCrazy();
-  console.log("hey!!!");
-  }
-}
-
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
-}
